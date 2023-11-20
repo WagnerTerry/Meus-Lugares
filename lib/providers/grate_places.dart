dd773312ee416f9ddf5521bca2ef012b8df9520a
Wagner Terry <goncalveswagner15@gmail.com>
2023-11-20 19:46:21 -0300

    Componente lista de lugares

--- lib/providers/grate_places.dart
+++ lib/providers/grate_places.dart
@@ -12,6 +12,10 @@ class GreatPlaces with ChangeNotifier {
     return [..._items];
   }
 
+  int get itemsCount {
+    return _items.length;
+  }
+
   Place itemByIndex(int index) {
     return _items[index];
   }
--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -1,22 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:meus_lugares/providers/grate_places.dart';
 import 'package:meus_lugares/utils/app_routes.dart';
+import 'package:provider/provider.dart';
 
 class PlaceListScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Meus Lugares'),
-        actions: <Widget>[
-          IconButton(
-              onPressed: () =>
-                  Navigator.of(context).pushNamed(AppRoutes.PLACE_FORM),
-              icon: const Icon(Icons.add))
-        ],
-      ),
-      body: Center(
-        child: CircularProgressIndicator(),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Meus Lugares'),
+          actions: <Widget>[
+            IconButton(
+                onPressed: () =>
+                    Navigator.of(context).pushNamed(AppRoutes.PLACE_FORM),
+                icon: const Icon(Icons.add))
+          ],
+        ),
+        body: Consumer<GreatPlaces>(
+            child: const Center(
+              child: Text('Nenhum local cadastrado!'),
+            ),
+            builder: (ctx, greatPlaces, ch) => greatPlaces.itemsCount == 0
+                ? ch!
+                : ListView.builder(
+                    itemCount: greatPlaces.itemsCount,
+                    itemBuilder: (ctx, i) => ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage:
+                            FileImage(greatPlaces.itemByIndex(i).image),
+                      ),
+                      title: Text(greatPlaces.itemByIndex(i).title),
+                      onTap: () {},
+                    ),
+                  )));
   }
 }
