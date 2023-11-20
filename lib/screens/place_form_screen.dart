d6e7611f25d8b82aa04e2be4b6ef5afdfc7ef016
Wagner Terry <goncalveswagner15@gmail.com>
2023-11-20 13:19:23 -0300

    Formulario novo lugar

--- lib/screens/place_form_screen.dart
+++ lib/screens/place_form_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:meus_lugares/widgets/image_input.dart';
 
 class PlaceFormScreen extends StatefulWidget {
   @override
@@ -6,14 +7,51 @@ class PlaceFormScreen extends StatefulWidget {
 }
 
 class _PlaceFormScreenState extends State<PlaceFormScreen> {
+  final _titleController = TextEditingController();
+
+  void _submitForm() {}
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Novo Lugar'),
       ),
-      body: const Center(
-        child: Text('Form'),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: _titleController,
+                      decoration: const InputDecoration(
+                        labelText: 'Título',
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    const ImageInput(),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          ElevatedButton.icon(
+            icon: const Icon(Icons.add),
+            label: const Text('Adicionar'),
+            style: ElevatedButton.styleFrom(
+              foregroundColor: Colors.black,
+              backgroundColor: Colors.amber,
+              // backgroundColor: Theme.of(context).colorScheme.secondary,
+              elevation: 0,
+              tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+              shape: const ContinuousRectangleBorder(),
+            ),
+            onPressed: _submitForm,
+          ),
+        ],
       ),
     );
   }
